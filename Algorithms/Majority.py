e2fadfd1a264432d25716787cf3b5bd03043699d
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2019-07-08 15:54:23 -0700

    files for the majority algo

---
+++ Algorithms/Majority.py
@@ -0,0 +1,5 @@
+'''
+The Majority algorithm proposed by Demaine et al.
+
+It's a generalization of the majority algorithm.
+'''
---
+++ Algorithms/MajorityTest.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, main
+from StreamManager.Stream import SimpleStream
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
